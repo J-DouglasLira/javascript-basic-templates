34ac93b0f36ece56f6ac6ebcf48efd2105e9366a
J-DouglasLira <jdouglira@gmail.com>
2022-01-25 15:30:21 -0300

    first commit

--- kataExercises/reversedStrings.js
+++ kataExercises/reversedStrings.js
@@ -12,7 +12,8 @@ function solution(str){
     return myNewStr;
 } 
 
-console.log(solution("doug"));
+console.log(solution("First solution"));
+
 
 //second solution 
 
@@ -20,8 +21,19 @@ function solutionSecondWay(string){
  return string.split("").reverse().join("");
 }
 
+console.log(solutionSecondWay("Second solution"));
+// Third solution; 
 
+function solutionNewWay(newString){
+  const input = newString.split("");
+  let reverse = [];
+  for (let i = 0; i<input.length; i++){
+    reverse.unshift(input[i]);
+  }
+  return reverse.join("");
+}
 
+console.log(solutionNewWay("Third solution"));
 
 
 
