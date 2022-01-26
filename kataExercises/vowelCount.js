d3fcf19b2794b7ad8aa5d5ecf9d5675e6086d6d6
J-DouglasLira <jdouglira@gmail.com>
2022-01-26 16:06:26 -0300

    first commit

--- kataExercises/vowelCount.js
+++ kataExercises/vowelCount.js
@@ -1,38 +1,43 @@
-
 function getCount(str) {
-  myNewStr = str.split(''); // convertendo em um array; [d.o.u.g]
+  myNewStr = str.split(""); // convertendo em um array; [d.o.u.g]
   var vowelsCount = 0;
-  for (let i = 0; i<myNewStr.length; i++) {
-    switch (myNewStr[i]){
-      case 'a' : vowelsCount+=1
-      break;
-      case 'e' : vowelsCount+=1
-      break;
-      case 'i' : vowelsCount+=1
-      break;
-      case 'o' : vowelsCount+=1
-      break;
-      case 'u' : vowelsCount+=1
-      default : vowelsCount=vowelsCount;
+  for (let i = 0; i < myNewStr.length; i++) {
+    switch (myNewStr[i]) {
+      case "a":
+        vowelsCount += 1;
+        break;
+      case "e":
+        vowelsCount += 1;
+        break;
+      case "i":
+        vowelsCount += 1;
+        break;
+      case "o":
+        vowelsCount += 1;
+        break;
+      case "u":
+        vowelsCount += 1;
+      default:
+        vowelsCount = vowelsCount;
     }
   }
   return vowelsCount;
 }
 
-console.log(getCount("douglas"));
+//console.log(getCount("douglas"));
 
-// second solution 
+// second solution
 
 let str2 = "douglas";
-function numbersOfVowels(str2){
-let myNewArray = str2.split("");
-var vowels = ["a","e","i","o","u"],
-    numbersOfVowels= myNewArray.filter(item => vowels.includes(item)).length;
-return numbersOfVowels;
+function numbersOfVowels(str2) {
+  var vowels = ["a", "e", "i", "o", "u"];
+  let myNewArray = str2.split("");
+  const callback = (currentString) =>
+  vowels.includes(currentString)
+  numbersOfVowels = myNewArray.filter(callback).length;
+  return numbersOfVowels;
 }
 
-console.log(numbersOfVowels(str2))
 
-
-
+console.log(numbersOfVowels(str2));
 
