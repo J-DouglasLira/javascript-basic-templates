489b50c573c059d0eb3bdacc702eda458447134b
J-DouglasLira <jdouglira@gmail.com>
2022-03-07 17:05:40 -0300

    first commit

---
+++ kataExercises/stopWatch.js
@@ -0,0 +1,18 @@
+
+function Stopwatch(){
+  let startTime, endTime, running, duration = 0;
+
+  this.start = function(){
+
+  };
+
+  this.stop = function() {
+
+  };
+
+  this.reset = function(){
+
+  };
+
+  Object.defineProperty(this,'duration',{...});
+}
